fbdc8143d0060419b9782a9974676ae0a865b4fb
Ornella <ornelladelvicario@gmail.com>
2025-04-23 11:11:13 +0200

    Changed printing methods in TrackerDemo for better viewing and understanding

--- src/main/TrackerDemo.java
+++ src/main/TrackerDemo.java
@@ -9,13 +9,13 @@ import java.util.List;
 
 public class TrackerDemo {
     public static void main(String[] args) {
-        // Create Different type of Objects
+        // Create different type of objects
         Book book = new Book("Java Programming", "John Doe");
         Device device = new Device("Smartphone", "Samsung");
         Snack snack = new Snack("Chips", "BBQ");
-        Perishable milk = new Perishable("Milk", false);
+        Perishable milk = new Perishable("Milk", false);  // La leche no está expirada
 
-        // Create Storage Units for each type
+        // Create StorageUnits for each type of object
         StorageUnit<Book> bookStorage = new StorageUnit<>();
         StorageUnit<Device> deviceStorage = new StorageUnit<>();
         StorageUnit<Snack> snackStorage = new StorageUnit<>();
@@ -27,20 +27,29 @@ public class TrackerDemo {
         snackStorage.addItem(snack);
         perishableStorage.addItem(milk);
 
-        // Show items
+        System.out.println("\n=== Easy Storage Tracker ===\n");
+
+        // Print first non-perishable items
+        System.out.println("Non-perishable items:\n");
         StorageUtils.displayItem(bookStorage.getItem());
         StorageUtils.displayItem(deviceStorage.getItem());
         StorageUtils.displayItem(snackStorage.getItem());
+
+        // Print now perishable items
+        System.out.println("\nPerishable item:\n");
         StorageUtils.displayItem(perishableStorage.getItem());
 
-        // Check Perishable Items
+        // Check if perishable items are expired
         StorageUtils.checkPerishable(milk);
 
-        // Wildcard Method
+        System.out.println("\n=== Wildcard Item List ===\n");
+        // Use Wildcard Method to print all items
         List<Object> items = new ArrayList<>();
         items.add(book);
         items.add(device);
         items.add(snack);
+        items.add(milk);  // Add milk
         StorageUtils.printItems(items);
     }
+
 }
