a2369d5b9b0b316fab73eeb058278f7bad5e79d6
Ornella <ornelladelvicario@gmail.com>
2025-04-23 10:53:58 +0200

    Added StorageUnit.java and StorageUtils.java in storage

--- src/storage/StorageUnit.java
+++ src/storage/StorageUnit.java
@@ -1,4 +1,12 @@
 package storage;
+    public class StorageUnit<T> {
+        private T item;
 
-public class StorageUnit {
-}
+        public void addItem(T item) {
+            this.item = item;
+        }
+
+        public T getItem() {
+            return item;
+        }
+    }
--- src/storage/StorageUtils.java
+++ src/storage/StorageUtils.java
@@ -1,4 +1,28 @@
 package storage;
 
+import model.Perishable;
+import java.util.List;
+
 public class StorageUtils {
+
+    // Wildcard Method
+    public static void printItems(List<? extends Object> items) {
+        for (Object item : items) {
+            System.out.println(item);
+        }
+    }
+
+    // Generic Method
+    public static <T> void displayItem(T item) {
+        System.out.println(item);
+    }
+
+    // Bounded Type Method
+    public static <T extends Perishable> void checkPerishable(T item) {
+        if (item.isExpired()) {
+            System.out.println(item.getName() + " is expired.");
+        } else {
+            System.out.println(item.getName() + " is still good.");
+        }
+    }
 }
